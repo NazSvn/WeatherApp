ab95e0a189c2de21e5426c7072e37e94f4cede9e
NazSvn <idler90@gmail.com>
2024-11-24 13:18:51 +0800

    feat(CurrentWeather): Add weather icon and improve temperature layout
    - Add WeatherIcon component to display visual weather status
    - Create new weather-temp-container for better organization
    - Add weather description next to temperature
    - Remove redundant weather code display
    - Improve layout structure for temperature and description section

--- src/components/CurrentWeather/CurrentWeather.jsx
+++ src/components/CurrentWeather/CurrentWeather.jsx
@@ -2,20 +2,36 @@ import PropTypes from 'prop-types';
 import { weatherCodeMap } from '../../services/weatherCodeMap';
 import './currentWeather.css';
 import getCardinalDirection from '../../services/windDirection';
+import WeatherIcon from '../../services/weatherIconMap';
 
 const CurrentWeather = ({ weatherData }) => {
   if (!weatherData || !weatherData.current) return null;
 
+  const weathercode = weatherData.current.weathercode;
+
   return (
     <>
       <div className='weather-data'>
-        <div className='weather-temp'>
-          <div className='weather-temp-value'>
-            {Math.round(weatherData.current.temperature_2m)}
+        <div className='weather-temp-container'>
+          <div className='weather-temp'>
+            <div className='weather-temp-value'>
+              {Math.round(weatherData.current.temperature_2m)}
+            </div>
+            <span className='weather-temp-unit'>
+              {weatherData.current_units.temperature_2m}
+            </span>
           </div>
-          <span className='weather-temp-unit'>
-            {weatherData.current_units.temperature_2m}
-          </span>
+          <div className='weather-temp-descr'>
+            <span className='weather-temp-text'>
+              {weatherCodeMap[weatherData.current.weathercode].description}
+            </span>
+            <span className='weather-icon'>
+              <WeatherIcon
+                weatherCode={weathercode}
+                size={50}
+              />
+            </span>
+          </div>
         </div>
         <div className='weather-data-rows'>
           <div className='weather-row'>
@@ -24,15 +40,7 @@ const CurrentWeather = ({ weatherData }) => {
               {Math.round(weatherData.current.apparent_temperature)}
               {weatherData.current_units.temperature_2m}
             </span>
-          </div>
-
-          <div className='weather-row'>
-            <span className='weather-row-descr'>Code:</span>
-            <span>
-              {weatherCodeMap[weatherData.current.weathercode].description}
-            </span>
-          </div>
-
+          </div> 
           <div className='weather-row'>
             <span className='weather-row-descr'>Humidity:</span>
             <span>
